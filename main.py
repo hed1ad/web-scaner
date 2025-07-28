f235363aa2bfe42e3838b3ada77244ae39551dc7
hed1ad <diad.hedi@gmail.com>
2025-07-28 22:31:26 +0300

    logo

---
+++ main.py
@@ -0,0 +1,26 @@
+def show_banner():
+    banner = r"""
+     _______  ______  __    __ 
+    |__   __||___  /  \ \  / / 
+       | |     / /    \ \/ /  
+       | |    / /      >  <   
+       | |   / /__    / /\ \  
+       |_|  /_____|  /_/  \_\  v1.0
+    """
+    
+    print("\033[1;36m" + banner + "\033[0m")
+    print("\033[1;32m{:=^60}\033[0m".format(" ZOV Vulnerability Scanner "))
+    print("\033[1;33m• Author:\033[0m hed1ad, erz")
+    print("\033[1;33m• GitHub:\033[0m github.com/hed1ad/zovscan")
+    print("\033[1;33m• Supported Checks:\033[0m")
+    print("  - SQL Injection (SQLi)")
+    print("  - Cross-Site Scripting (XSS)")
+    print("  - Directory Traversal")
+    print("  - Insecure Headers")
+    print("  - Exposed Admin Panels")
+    print("\033[1;32m{:=^60}\033[0m".format(""))
+    print("Usage: zovscan --target https://example.com --scan all\n")
+    print("Press CTRL+C to stop scanning at any time")
+
+if __name__ == "__main__":
+    show_banner()
