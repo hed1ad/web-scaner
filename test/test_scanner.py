e3607d39110065fa2b2a18119013e1d1da073796
hed1ad <diad.hedi@gmail.com>
2025-07-30 22:50:13 +0300

    add simple test

---
+++ test/test_scanner.py
@@ -0,0 +1,22 @@
+import pytest
+from dir_scanner import DirScanner
+
+@pytest.fixture
+def mock_wordlist(tmp_path):
+    wordlist_file = tmp_path / "wordlist.txt"
+    wordlist_file.write_text("images")
+    return str(wordlist_file)
+
+@pytest.fixture
+def scanner(mock_wordlist):
+    return DirScanner(base_url="https://ya.ru", wordlist_path=mock_wordlist)
+
+def test_scan_real_request(scanner, capsys):
+    """Тестирует реальный HTTP-запрос без моков"""
+    scanner.scan()
+    
+    captured = capsys.readouterr()
+    assert any(
+        code in captured.out 
+        for code in ["[200]", "[403]", "[404]", "[30"]
+    )
